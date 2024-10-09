9941bc5afdb74b94da73f0f8715702f08de4e95b
LinuxSan <12554863+LinuxSan@users.noreply.github.com>
2024-10-09 14:21:34 +0200

    Add files via upload

---
+++ source.js
@@ -0,0 +1,208 @@
+
+var hoverEffect = true; // set true for hover effect, set false for no hover effect
+
+var searchEngine = 'google'; // default search engine - set google for google search, bing for bing search, yahoo for yahoo search
+
+var numberOfScreens = 3; // set number of screens (1 or 2 or 3)
+ 
+var blockName = new Array(); // set names of blocks
+
+blockName[1] = 'Most Used';
+blockName[2] = 'Stuff';
+blockName[3] = 'Newz & Stuff';
+
+var bookmark = new Array();
+bookmark[0] = new Array();
+bookmark[1] = new Array();
+bookmark[2] = new Array();
+
+
+// set your bookmarks here: (If you do not fill 'thumb' for thumbnail will be used title)
+// 1. BLOCK
+bookmark[0][0] = {
+'title':'Gizmodo',
+'url':'http://blog.gizmodo.com',
+'thumb':''
+};
+bookmark[0][1] = {
+'title':'LifeHacker',
+'url':'http://blog.lifehacker.com',
+'thumb':'lifehacker1.png'
+};
+bookmark[0][2] = {
+'title':'Newz',
+'url':'http://Newz.dk',
+'thumb':''
+};
+bookmark[0][3] = {
+'title':'Engadget',
+'url':'http://www.engadget.com/',
+'thumb':'engadget1.png'
+};
+bookmark[0][4] = {
+'title':'Reddit',
+'url':'https://www.reddit.com/',
+'thumb':'reddit.png'
+};
+bookmark[0][5] = {
+'title':'SDU',
+'url':'https://sso.sdu.dk/',
+'thumb':'6-1.png'
+};
+bookmark[0][6] = {
+'title':'RapidMoviez',
+'url':'http://rapidmoviez.com/',
+'thumb':''
+};
+bookmark[0][7] = {
+'title':'HorribleSubs',
+'url':'http://horriblesubs.info/',
+'thumb':''
+};
+bookmark[0][8] = {
+'title':'EkstraBladet',
+'url':'http://Ekstrabladet.dk',
+'thumb':''
+};
+bookmark[0][9] = {
+'title':'YouTube',
+'url':'https://www.youtube.com/index?gl=US&hl=en',
+'thumb':'youtube.png'
+};
+bookmark[0][10] = {
+'title':'SDU Mail',
+'url':'https://webmail.sdu.dk/owa/',
+'thumb':''
+};
+bookmark[0][11] = {
+'title':'Black Board',
+'url':'https://e-learn.sdu.dk/',
+'thumb':''
+};
+// end of 1. BLOCK
+// 2. BLOCK
+bookmark[1][0] = {
+'title':'MyUpload',
+'url':'http://myupload.dk/',
+'thumb':''
+};
+bookmark[1][1] = {
+'title':'ComON',
+'url':'http://www.comon.dk/',
+'thumb':''
+};
+bookmark[1][2] = {
+'title':'TheCHIVE',
+'url':'http://thechive.com/',
+'thumb':''
+};
+bookmark[1][3] = {
+'title':'msn',
+'url':'http://www.msn.com/',
+'thumb':'msn.png'
+};
+bookmark[1][4] = {
+'title':'9GAG',
+'url':'https://9gag.com',
+'thumb':''
+};
+bookmark[1][5] = {
+'title':'NextGen',
+'url':'https://nxtgn.org/login.php',
+'thumb':''
+};
+bookmark[1][6] = {
+'title':'Warez-DK',
+'url':'http://warez-dk.org/',
+'thumb':''
+};
+bookmark[1][7] = {
+'title':'NeoWin',
+'url':'http://www.neowin.net/forum/index.php',
+'thumb':''
+};
+bookmark[1][8] = {
+'title':'How to Geek',
+'url':'http://www.howtogeek.com',
+'thumb':''
+};
+bookmark[1][9] = {
+'title':'Hacker News',
+'url':'http://thehackernews.com/#_',
+'thumb':''
+};
+bookmark[1][10] = {
+'title':'XDA-Developer',
+'url':'http://forum.xda-developers.com/galaxy-s3?styleid=22',
+'thumb':''
+};
+bookmark[1][11] = {
+'title':'Giveaway of the Day',
+'url':'http://www.giveawayoftheday.com/',
+'thumb':''
+};
+// end of 2. BLOCK
+// 3. BLOCK
+bookmark[2][0] = {
+'title':'Evilzone',
+'url':'http://evilzone.org/',
+'thumb':''
+};
+bookmark[2][1] = {
+'title':'Hack a Day',
+'url':'http://hackaday.com',
+'thumb':''
+};
+bookmark[2][2] = {
+'title':'ITTIHack',
+'url':'http://www.ittihack.com',
+'thumb':''
+};
+bookmark[2][3] = {
+'title':'SupportForums',
+'url':'http://www.supportforums.net',
+'thumb':''
+};
+bookmark[2][4] = {
+'title':'Wallbase',
+'url':'http://wallbase.cc/',
+'thumb':''
+};
+bookmark[2][5] = {
+'title':'Hack Forums',
+'url':'http://www.hackforums.net/',
+'thumb':''
+};
+bookmark[2][6] = {
+'title':'C0rrupt',
+'url':'http://www.c0rrupt.net/forum/',
+'thumb':''
+};
+bookmark[2][7] = {
+'title':'BlackHatWorld',
+'url':'http://www.blackhatworld.com/blackhat-seo/',
+'thumb':''
+};
+bookmark[2][8] = {
+'title':'SecurityTube',
+'url':'http://www.securitytube.net',
+'thumb':''
+};
+bookmark[2][9] = {
+'title':'Wired',
+'url':'http://www.wired.com/',
+'thumb':''
+};
+bookmark[2][10] = {
+'title':'PasteBin',
+'url':'http://pastebin.com',
+'thumb':''
+};
+bookmark[2][11] = {
+'title':'PasteHtml',
+'url':'http://pastehtml.com/',
+'thumb':''
+};
+// end of 3. BLOCK
+    
+	
